0ca9f24be205366084bdab9aab32c6f706b60f49
waleedzaman7860 <105785746+waleedzaman7860@users.noreply.github.com>
2022-12-14 14:37:47 +0500

    hover-dropdown-javascript

---
+++ Javascript-Jquery/javascript-dropdown-menu-on-hover.js
@@ -0,0 +1,25 @@
+// Show header theme cards on Self-Help Menu Item hover
+let self_help_menu_item = document.querySelector('#menu-wellbeing_menu :nth-child(2)');;
+
+var setTimeToHide_ID;
+var timeOutValue = 300;
+const headerThemesPopup = document.querySelector('.themes_popup_header');
+self_help_menu_item.addEventListener('mouseover', function handleMouseOver() {
+    headerThemesPopup.classList.add('show');
+});
+headerThemesPopup.addEventListener('mouseover', function handleMouseOver(e) {
+    if (setTimeToHide_ID) {
+        window.clearTimeout(setTimeToHide_ID);
+        setTimeToHide_ID = 0;
+    }
+});
+self_help_menu_item.addEventListener('mouseout', function handleMouseOver() {
+    setTimeToHide_ID = window.setTimeout(function() {
+        headerThemesPopup.classList.remove('show');
+    }, timeOutValue);
+});
+headerThemesPopup.addEventListener('mouseout', function handleMouseOver() {
+    setTimeToHide_ID = window.setTimeout(function() {
+        headerThemesPopup.classList.remove('show');
+    }, timeOutValue);
+});
