69ee58ad4499fa9868321562a4c4266e0a2c09b1
Zohaib Ahmad <37594393+zohaibahmad969@users.noreply.github.com>
2024-05-08 10:22:22 +0500

    Update 15) menu links to page sections with fixed header.js

--- Wordpress/15) menu links to page sections with fixed header.js
+++ Wordpress/15) menu links to page sections with fixed header.js
@@ -1,15 +1,37 @@
-
-	jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 		$('.website-main-menu a[href^="#"]').on('click', function(event) {
+			let crnt = $(this);
 			var target = $($(this).attr('href'));
-			if (target.length) {
-				event.preventDefault();
-				var scrollOffset = $(window).width() < 768 ? 110 : 150; // Adjust offset for mobile
+			
+			event.preventDefault();
+			var scrollOffset = $(window).width() < 768 ? 110 : 150; // Adjust offset for mobile
+			
+			if ($("body").hasClass('home')) {
+				
 				$("#ha-megamenu-main-menu").removeClass('active');
 				$('html, body').stop().animate({
 					scrollTop: target.offset().top - scrollOffset
 				}, 1000);
+				
+			} else {
+				window.location.href = '/' + crnt.attr('href');
 			}
+			
 			return false; // Prevent default behavior of the anchor link
 		});
+		
+		
+		// Check if the URL contains a hash
+		if (window.location.hash) {
+			var targetId = window.location.hash.substring(1); // Extract section ID from the URL
+			var target = $("#" + targetId); // Get the target section by ID
+			if (target.length) {
+				var scrollOffset = $(window).width() < 768 ? 110 : 150; // Adjust offset for mobile
+				$('html, body').stop().animate({
+					scrollTop: target.offset().top - scrollOffset
+				}, 1000);
+			}
+		}
+
+
 	});
